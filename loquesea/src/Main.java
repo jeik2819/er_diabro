383ea4c77166574255876c63d101e0098c7320d5
jeik2819 <jeiko6@hotmail.es>
2026-05-07 11:27:27 +0200

    opa opa opa

--- loquesea/src/Main.java
+++ loquesea/src/Main.java
@@ -1,13 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.Scanner;
+    public static boolean er_diabro(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-}
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce un número: ");
+        int numero = scanner.nextInt();
+
+        if (er_diabro(numero)) {
+            System.out.println("El número " + numero + " es primo");
+        } else {
+            System.out.println("El número " + numero + " NO es primo");
+        }
+
+        scanner.close();
+    }
